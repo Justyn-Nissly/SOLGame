aaf2105fdb9682f3e664e71f2bc0b2f415f8dd11
Brett Lane <commodore@brettlane.com>
2019-11-01 07:35:22 -0600

    Player Movement Script
    Set the new movement script to be used by the Player prefab.

--- SOL_Game/Assets/Scripts/GameMechanics/PlayerMechanics/Movement/playerMovement.cs
+++ SOL_Game/Assets/Scripts/GameMechanics/PlayerMechanics/Movement/playerMovement.cs
@@ -10,13 +10,14 @@ public class playerMovement : MonoBehaviour
 	private float moveVertical;
 	private Vector2 movement;
 	private Rigidbody2D rb2d;
+    public Animator animator;
 
-	
 
-	/// <summary>
-	/// Start is called before the first frame update
-	/// </summary>
-	void Start()
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
     {
 		speed = .1f;
 
@@ -33,7 +34,7 @@ public class playerMovement : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		// get the horizontal movement input
+		// Get the Horizontal Axis
 		if (Input.GetAxis("Horizontal") > 0)
 			moveHorizontal = speed;
 		else if (Input.GetAxis("Horizontal") < 0)
@@ -41,7 +42,7 @@ public class playerMovement : MonoBehaviour
 		else
 			moveHorizontal = 0;
 
-		// get the vertical movement input
+		// Get the Vertical Axis
 		if (Input.GetAxis("Vertical") > 0)
 			moveVertical = speed;
 		else if (Input.GetAxis("Vertical") < 0)
@@ -49,14 +50,15 @@ public class playerMovement : MonoBehaviour
 		else
 			moveVertical = 0;
 
+        // Set the movement vector
 		movement = new Vector2(moveHorizontal, moveVertical);
 
-		// move the player if there not colliding with a object
+        // Update the values in the Animator
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", speed);
 
-		rb2d.MovePosition(movement + rb2d.position);
-//        if (Input.GetKeyDown(KeyCode.Escape))
-//        {
-//            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-//        }
+        // Update the Hero's position, taking note of colliders.
+        rb2d.MovePosition(movement + rb2d.position);
 	}
 }
