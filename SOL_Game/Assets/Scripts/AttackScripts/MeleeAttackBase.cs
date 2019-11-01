b626301f03444a28ecb813b747aa1bc37b50a65c
danieldefiancegamestudio <daniel@defiancegamestudio.com>
2019-10-31 20:54:03 -0500

    disabled knock back
    Temparly disabled knockback because its unfinished an was causing problems.

--- SOL_Game/Assets/Scripts/AttackScripts/MeleeAttackBase.cs
+++ SOL_Game/Assets/Scripts/AttackScripts/MeleeAttackBase.cs
@@ -41,7 +41,7 @@ public class MeleeAttackBase : MonoBehaviour
 				characterBeingAtacked.TakeDamage(damage);
 
 
-				ApplyKnockBack(collider.gameObject);
+				//ApplyKnockBack(collider.gameObject);
 
 			}
 		}
