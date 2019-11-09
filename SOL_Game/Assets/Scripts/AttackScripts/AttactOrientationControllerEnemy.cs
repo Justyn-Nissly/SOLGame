345627a533d3a7fab24072806e814f43d8705e12
danieldefiancegamestudio <daniel@defiancegamestudio.com>
2019-11-09 10:52:33 -0600

    fixed the prefabs

---
+++ SOL_Game/Assets/Scripts/AttackScripts/AttactOrientationControllerEnemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttactOrientationControllerEnemy : MonoBehaviour
+{
+	#region Enums
+	#endregion
+
+	#region Public Variables
+	#endregion
+
+	#region Private Variables
+	private GameObject gameObjectToLookAt;
+	#endregion
+
+	// Unity Named Methods
+	#region Main Methods
+	private void FixedUpdate()
+	{
+		if (gameObjectToLookAt != null)
+		{
+			LookInEightDirectionOfGameObject();
+		}
+	}
+
+	private void Start()
+	{
+		gameObjectToLookAt = GameObject.FindGameObjectWithTag("Player").gameObject;
+	}
+	#endregion
+
+	#region Utility Methods
+	public void LookInEightDirectionOfGameObject()
+	{
+		// This makes the game object that this script is attached to rotate on the z axis to look at the game object to look at
+		// and only lets that game object "look" in eight directions
+		Vector3 dir = gameObjectToLookAt.transform.position - transform.position;
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 22.5f;
+
+
+		if (angle < 0f)
+		{
+			angle += 360f;
+		}
+		else if (angle >= 360f)
+		{
+			angle -= 360f;
+		}
+
+		// Make positive if negative
+		if (angle < 0)
+		{
+			angle += 360;
+		}
+
+		angle = (float)(((int)angle) / 45) * 45.0f;
+
+		// apply rotation
+		transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+	}
+	#endregion
+
+	#region Coroutines
+	#endregion
+}
