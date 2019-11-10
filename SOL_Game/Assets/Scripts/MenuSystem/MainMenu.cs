7fa64bfa7728dc71e2279272cf10285b698eb9c4
Justyn Nissly <jnissly98@gmail.com>
2019-11-10 16:39:55 -0600

    Revert "Update MainMenu.cs"
    This reverts commit a889488b48fa595ede8955c95824fa6bcd4fc7ea.

--- SOL_Game/Assets/Scripts/MenuSystem/MainMenu.cs
+++ SOL_Game/Assets/Scripts/MenuSystem/MainMenu.cs
@@ -5,21 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-	#region Enums
-
-	#endregion
-
-	#region Public Variables
-
-	#endregion
-
-	#region Private Variables
 	private GameObject pauseMenu;
-	#endregion
-
-	// Unity Named Methods
-	#region Main Methods
-	/// Check every frame for the "end" key to open to developer menu
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.End))
@@ -27,24 +13,19 @@ public class MainMenu : MonoBehaviour
 			SceneManager.LoadScene("DevMenu");
 		}
 	}
-	#endregion
-
-	#region Utility Methods
-	/// Load the game
+	// Loads the game
 	public void PlayGame()
-	{
-		SceneManager.LoadScene("Hub");
+    {
+        SceneManager.LoadScene("Hub");
+		pauseMenu = GameObject.Find("PauseMenuCanvas(Clone)");
+		pauseMenu.GetComponent<Canvas>().enabled = false;
 	}
 
-	/// Quit the game
-	public void QuitGame()
-	{
-		Debug.Log("QUIT");
-		Application.Quit();
-	}
-	#endregion
 
-	#region Coroutines
-
-	#endregion
+    // Quits the game
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
 }
