a889488b48fa595ede8955c95824fa6bcd4fc7ea
Justyn Nissly <jnissly98@gmail.com>
2019-11-10 16:39:29 -0600

    Update MainMenu.cs
    Updated the main menu script to be within our style, fixed documentation, and fixed the pause menu not being deactivated when the user quits back to the main menu.

--- SOL_Game/Assets/Scripts/MenuSystem/MainMenu.cs
+++ SOL_Game/Assets/Scripts/MenuSystem/MainMenu.cs
@@ -5,7 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+	#region Enums
+
+	#endregion
+
+	#region Public Variables
+
+	#endregion
+
+	#region Private Variables
 	private GameObject pauseMenu;
+	#endregion
+
+	// Unity Named Methods
+	#region Main Methods
+	/// Check every frame for the "end" key to open to developer menu
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.End))
@@ -13,19 +27,24 @@ public class MainMenu : MonoBehaviour
 			SceneManager.LoadScene("DevMenu");
 		}
 	}
-	// Loads the game
+	#endregion
+
+	#region Utility Methods
+	/// Load the game
 	public void PlayGame()
-    {
-        SceneManager.LoadScene("Hub");
-		pauseMenu = GameObject.Find("PauseMenuCanvas(Clone)");
-		pauseMenu.GetComponent<Canvas>().enabled = false;
+	{
+		SceneManager.LoadScene("Hub");
 	}
 
+	/// Quit the game
+	public void QuitGame()
+	{
+		Debug.Log("QUIT");
+		Application.Quit();
+	}
+	#endregion
 
-    // Quits the game
-    public void QuitGame()
-    {
-        Debug.Log("QUIT");
-        Application.Quit();
-    }
+	#region Coroutines
+
+	#endregion
 }
