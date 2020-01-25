245dc263f5bf78d1890c070623de30cbfa5986fb
danieldefiancegamestudio <daniel@defiancegamestudio.com>
2020-01-25 14:44:31 -0600

    changed the destructible wall script to be a destructible object script
    Added that you can choose what player weapon can destroy this game object.
    
    Added an optional list of doors that get unlocked when this game object is destroyed.
    
    Added a new tag for the players bullet.

---
+++ SOL_Game/Assets/Scripts/GameMechanics/SceneManagement/DestructibleObject.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+	#region Enums (Empty)
+	[System.Serializable]
+	public enum WeaponTag
+	{
+		HeavyMelee,
+		LightMelee,
+		Ranged,
+	}
+	#endregion
+
+	#region Public Variables
+	public WeaponTag weaponDestroysObject;
+	public List<DoorLogic> doorsUnlocked = new List<DoorLogic>(); // all door that will be unlocked when this destructible object is destroyed (this list can be empty)
+	#endregion
+
+	#region Private Variables
+	private DoorManager doorManager = new DoorManager(); // this manager has the logic to control a list of door (in this case used to unlock them all)
+	#endregion
+
+	// Unity Named Methods
+	#region Main Methods
+
+	private void Start()
+	{
+		// add all doors to the door manager and lock all the doors because they should be locked
+		doorManager.doors.AddRange(doorsUnlocked);
+		doorManager.LockAllDoors();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag(ConvertTagToString(weaponDestroysObject)))
+		{
+			// unlock any doors that need to be unlocked
+			doorManager.UnlockAllDoors();
+
+			// destroy this gameobject
+			Destroy(gameObject);
+		}
+	}
+	#endregion
+
+	#region Utility Methods (Empty)
+	private string ConvertTagToString(WeaponTag weaponTag)
+	{
+		string weaponTagString;
+
+		if (weaponTag == WeaponTag.Ranged)
+		{
+			weaponTagString = "PlayerRangedWeapon";
+		}
+		else if (weaponTag == WeaponTag.LightMelee)
+		{
+			weaponTagString = "PlayerLightWeapon";
+		} 
+		else
+		{
+			weaponTagString = "PlayerHeavyWeapon";
+		}
+
+
+		return weaponTagString;
+	}
+	#endregion
+
+	#region Coroutines (Empty)
+	#endregion
+}
