ce0deeb376e52bc088566e1a5b66408e573000eb
OrzulianTungsten <57021047+OrzulianTungsten@users.noreply.github.com>
2020-02-06 23:51:52 -0600

    Added conveyor belt control
    Conveyor belt direction can switch and I added some picture files, but they probably aren't really needed

---
+++ SOL_Game/Assets/Scripts/GameMechanics/SceneManagement/ConveryorBeltControl.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ConveryorBeltControl : MonoBehaviour
+{
+	#region Enums (Empty)
+	#endregion
+
+	#region Public Variables (Empty)
+	#endregion
+
+	#region Private Variables
+	private bool
+		reversed;
+	private ConveyorBelt[]
+		belts;
+	private SpriteRenderer
+		sprite; // The belt control lever
+	private float
+		coolDownTime,  // Time left until the lever can be flipped again
+		coolDownTimer; // Time until the lever can be flipped again
+	private AudioSource
+		soundEffect; // Played when the lever flips
+	#endregion
+
+	// Unity Named Methods
+	#region Main Methods
+	// Initalize the missile
+	void Start()
+	{
+		reversed      = false;
+		belts         = FindObjectsOfType<ConveyorBelt>();
+		sprite        = GetComponent<SpriteRenderer>();
+		soundEffect   = GetComponent<AudioSource>();
+		coolDownTime  = 0.0f;
+		coolDownTimer = 1.0f;
+	}
+
+	/// <summary> Lock on to the target and arc towards it </summary>
+	void FixedUpdate()
+	{
+		if (reversed && coolDownTime <= 0.0f)
+		{
+			SwitchBeltDirection();
+			coolDownTime = coolDownTimer;
+			reversed     = !reversed;
+			sprite.flipX = !sprite.flipX;
+			soundEffect.Play();
+		}
+		else
+		{
+			coolDownTime -= Time.deltaTime;
+		}
+	}
+
+	/// <summary> Damage the player on contact and destroy the missile </summary>
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			reversed = true;
+		}
+	}
+	#endregion
+
+	#region Utility Methods
+	void SwitchBeltDirection()
+	{
+		foreach (ConveyorBelt conveyorBelt in belts)
+		{
+			switch (conveyorBelt.direction)
+			{
+				case ConveyorBelt.Direction.Up:
+					conveyorBelt.direction = ConveyorBelt.Direction.Down;
+					break;
+				case ConveyorBelt.Direction.Down:
+					conveyorBelt.direction = ConveyorBelt.Direction.Up;
+					break;
+				case ConveyorBelt.Direction.Left:
+					conveyorBelt.direction = ConveyorBelt.Direction.Right;
+					break;
+				case ConveyorBelt.Direction.Right:
+					conveyorBelt.direction = ConveyorBelt.Direction.Left;
+					break;
+				default:
+					Debug.Log("No direction set.");
+					break;
+			}
+		}
+	}
+	#endregion
+
+	#region Coroutines (Empty)
+	#endregion
+}
---
+++ SOL_Game/Assets/Scripts/GameMechanics/SceneManagement/ConveyorBelt.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+	#region Enums
+	public enum Direction
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	}
+	#endregion
+
+	#region Public Variables
+	[Header("Keep at least 300.")]
+	public float
+		speed; // How fast the conveyor belt pushes things
+	public Direction
+		direction;
+	public Vector2
+		movement;
+	#endregion
+
+	#region Private Variables
+	private bool 
+		isMoving;
+	#endregion
+
+	// Unity Named Methods
+	#region Main Methods
+	void Start()
+	{
+		isMoving = true;
+	}
+
+	void Update()
+	{
+		switch (direction)
+		{
+			case Direction.Up:
+				movement = Vector2.up;
+				break;
+			case Direction.Down:
+				movement = Vector2.down;
+				break;
+			case Direction.Left:
+				movement = Vector2.left;
+				break;
+			case Direction.Right:
+				movement = Vector2.right;
+				break;
+			default:
+				Debug.Log("No direction set.");
+				break;
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collider)
+	{
+		// if the player collides with this trigger spawn in enemies
+		if (collider.gameObject.CompareTag("Player") || collider.gameObject.CompareTag("Enemy") && isMoving)
+		{
+			collider.attachedRigidbody.AddRelativeForce(movement * speed, ForceMode2D.Force);
+		}
+	}
+	#endregion
+
+	#region Utility Methods (Empty)
+	#endregion
+
+	#region Coroutines (Empty)
+	#endregion
+}
