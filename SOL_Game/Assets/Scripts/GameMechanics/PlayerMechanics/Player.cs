ae1b528f596b2bca44651774879bc919304352d2
OrzulianTungsten <57021047+OrzulianTungsten@users.noreply.github.com>
2020-03-16 14:19:27 -0500

    Revert "fixed that the hammer can destroy objects"
    This reverts commit 85c9abd50522ae217bdd5da40a0bc04fcb837ec8.

--- SOL_Game/Assets/Scripts/GameMechanics/PlayerMechanics/Player.cs
+++ SOL_Game/Assets/Scripts/GameMechanics/PlayerMechanics/Player.cs
@@ -282,7 +282,7 @@ public class Player : BaseCharacter
 	public void DealPlayerHammerDamage()
 	{
 		// Deal Player hammer Damage to enemies in range
-		MeleeAttack(heavyMeleeWeapon, heavyMeleeAttackPosition, heavyMeleeAttackRange, heavyMeleeDamageToGive, true);
+		MeleeAttack(heavyMeleeWeapon, heavyMeleeAttackPosition, heavyMeleeAttackRange, heavyMeleeDamageToGive, false);
 	}
 
 	/// <summary> shake the screen (called from in event in the hammer animation) </summary>
